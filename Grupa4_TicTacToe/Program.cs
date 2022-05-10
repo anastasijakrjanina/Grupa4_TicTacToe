96ca38bdf642324cc66735241392fee2f7fd0af9
DaceVil <vilcina.dace@gmail.com>
2022-05-10 21:40:11 +0300

    10.05.22

--- Grupa4_TicTacToe/Program.cs
+++ Grupa4_TicTacToe/Program.cs
@@ -1,1 +1,29 @@
-﻿//1. hsdhjb
+﻿//1. Noteikt dalībnieku skaitu un tipu:
+//spēlētāju skaits 2 :
+//spēlētāju tips:
+// game with friend
+// random player
+// computer
+
+//2.izvēlas simbolu
+//
+//spēlētājs izvēlas spēles simboliu (X vai O)
+//
+
+
+//3.Izvēlas ***spēles līmeņi - viegls (3x3), vidējs , sarežģīts
+
+//4. Spēles noteikumi
+//4.1 Sāk spēlētājs ar simbolu X, var veikt tikai vienu gājiemu viena reizē, gājiens pāriet nākošajam spēlētājam
+//4.2 Izdarīto gājienu nedrīkst mainīt
+//4.3 Nedrīkst izmantot jau aizpildītu lauciņu
+//4.4 uzvar spelētājs , kurš pirmais savāc 3 savus simbolus vai nu horizontāli, vai vertikāli, vai pa diognāli
+//4.5 Apsveic uzvarētāju
+
+//5. Spēle beigusies
+//5.1 Sākt jaunu spēli vai turpināt ar to pašu spēlētāju (ja turpina ar to pašu spēlētāju, var izvēlēties simbolu)
+
+//
+//
+
+
