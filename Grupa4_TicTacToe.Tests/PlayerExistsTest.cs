aed53aad085a2b3a99e3c5c66eb502d0435e7ba0
DaceVil <103066879+DaceVil@users.noreply.github.com>
2022-05-31 13:49:53 +0200

    Update PlayerExistsTest.cs

--- Grupa4_TicTacToe.Tests/PlayerExistsTest.cs
+++ Grupa4_TicTacToe.Tests/PlayerExistsTest.cs
@@ -3,15 +3,39 @@ using Xunit;
 
 namespace Grupa4_TicTacToe.Tests
 {
-    public class PlayerExistsTest
+    public class PlayersTest
     {
+        private string num;
+
         [Fact]
-        public void GetPlayerString_WhenPlayersNameEntered_ThenReturnsTrue()  
+        //public void GetPlayerString_WhenPlayers1NameEntered_ThenReturnsTrue()  
+        //{
+        //    bool response = Players.IsPlayer1NameExist();
+
+        //    Assert.Equal("true", response);
+        //}
+
+        public void GetPlayerString_WhenPlayers1NameEntered_ThenReturnsYes()
         {
-            string response = Players.GetPlayerString();
+            var Players = new Players();
+            string response = Players.IsPlayer1NameExist(1);
+
+              Assert.Equal("True", response);
+
+        }
+
+        [Fact]
+        public void GetPlayerString_WhenPlayers2NameEntered_ThenReturnsYes()
+        {
+            string response = Players.IsPlayer2NameExist(2);
 
             Assert.Equal("True", response);
+
         }
+
+        //[Theory]
+        //[InlineData()]
+        //[InlineData()]
 
     }
 }
