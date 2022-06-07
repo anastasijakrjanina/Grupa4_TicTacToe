65d3fcc1b53c2df552fb6af1aa64f361d98d3f35
jevgmeln <36517545+jevgmeln@users.noreply.github.com>
2022-06-07 20:42:55 +0300

    New changes

--- Grupa4_TicTacToe/Program.cs
+++ Grupa4_TicTacToe/Program.cs
@@ -1,62 +1,62 @@
-﻿//1. Noteikt dalībnieku skaitu un tipu:
+//1. Noteikt dalībnieku skaitu un tipu:
 //spēlētāju skaits 2 :
 //spēlētāju tips:
 // game with friend  // labāk uztaisīt tikai vienu no sākuma, piemēram, cilvēks pret cilvēku
 // -random player
 // -computer
-
+ 
 //2.izvēlas simbolu // good to have // Labāk uzreiz ņemt default X, bez izvēles
 //
 //spēlētājs izvēlas spēles simboliu (X vai O)
 //
-
-
+ 
+ 
 //3.Izvēlas *spēles līmeņi - viegls (3x3)
-
+ 
 //4. Spēles noteikumi
 //4.1 Sāk spēlētājs ar simbolu X, var veikt tikai vienu gājiemu viena reizē, gājiens pāriet nākošajam spēlētājam
 //4.2 Izdarīto gājienu nedrīkst mainīt
 //4.3 Nedrīkst izmantot jau aizpildītu lauciņu
 //4.4 uzvar spelētājs , kurš pirmais savāc 3 savus simbolus vai nu horizontāli, vai vertikāli, vai pa diognāli
 //4.5 Apsveic uzvarētāju
-
+ 
 //5. Spēle beigusies
 //5.1 Sākt jaunu spēli vai turpināt ar to pašu spēlētāju (ja turpina ar to pašu spēlētāju, var izvēlēties simbolu)
-
+ 
 //
 //
-
-
+ 
+ 
 //kā saukt lauciņus:
 //A1 etc, 1-9, xrinda y kolonna
-
-
+ 
+ 
 using Grupa4_TicTacToe;
 string helloPlayer = GameIntro.HelloPlayer;
 Console.WriteLine(helloPlayer);
-
+ 
 GameIntro.GameRules();
-
-
+ 
+ 
 GameIntro.GoodLuck();
-
+ 
 Players players = new Players();
-
-
+ 
+ 
 Console.WriteLine();
 Console.Write("Player 1, please enter Your name:");
 Console.WriteLine();
 players.Person1 = Console.ReadLine();
-
+ 
 if (string.IsNullOrWhiteSpace(players.Person1))
 {
     Console.WriteLine("You did not enter your name. Try again.");
     players.Person1 = Console.ReadLine();
-
+ 
     if (string.IsNullOrWhiteSpace(players.Person1))
     {
         Console.WriteLine("No more tries");
-
+ 
     }
     else
     {
@@ -67,23 +67,23 @@ else
 {
     Console.WriteLine($"Hi {players.Person1}.");
 }
-
-
+ 
+ 
 Console.WriteLine();
 Console.Write("Player 2, please enter Your name:");
 Console.WriteLine();
 players.Person2 = Console.ReadLine();
-
+ 
 if (string.IsNullOrWhiteSpace(players.Person2))
 {
     Console.WriteLine("You did not enter your name. Try again.");
-
+ 
     players.Person2 = Console.ReadLine();
-
+ 
     if (string.IsNullOrWhiteSpace(players.Person2))
     {
         Console.WriteLine("No more tries");
-
+ 
     }
     else
     {
@@ -94,7 +94,198 @@ else
 {
     Console.WriteLine($"Your name is {players.Person2}.");
 }
-
+ 
 Console.WriteLine($"Player {players.Person1} is playing with {Symbol.X}");
-
+ 
 Console.WriteLine($"Player {players.Person2} is playing with {Symbol.O}");
+ 
+ 
+ 
+//Board started - addded 2022-06-06
+//Need to add different conditions, and make a loop and maybe move to a separate class
+ 
+string[] arr = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//Board1
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person1}");
+int addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "X";
+Console.Clear();
+ 
+//Board2
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person2}");
+addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "O";
+Console.Clear();
+//Board3
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person1}");
+addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "X";
+Console.Clear();
+ 
+//Board4
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person2}");
+addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "O";
+Console.Clear();
+ 
+//Board5
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person1}");
+addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "X";
+Console.Clear();
+ 
+//Board6
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person2}");
+addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "O";
+Console.Clear();
+ 
+ 
+//Board7
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person1}");
+addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "X";
+Console.Clear();
+ 
+//Board8
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person2}");
+addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "O";
+Console.Clear();
+ 
+//Board9
+{
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[0], arr[1], arr[2]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[3], arr[4], arr[5]);
+    Console.WriteLine("_____|_____|_____ ");
+    Console.WriteLine("     |     |      ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", arr[6], arr[7], arr[8]);
+    Console.WriteLine("     |     |      ");
+}
+ 
+Console.WriteLine($"Enter your value from 1 to 9: {players.Person1}");
+addedSymbol = int.Parse(Console.ReadLine());
+arr[addedSymbol - 1] = "X";
+Console.Clear();
+ 
+if (arr[0] == arr[1] && arr[1] == arr[2])
+{
+    Console.WriteLine($"Congratulations {players.Person1}!");
+    Console.WriteLine("You WON this game!");
+}
+else if (arr[3] == arr[4] && arr[4] == arr[5])
+{
+    Console.WriteLine($"Congratulations {players.Person1}!");
+    Console.WriteLine("You WON this game!");
+ 
+}
+ 
+else if (arr[6] == arr[7] && arr[7] == arr[8])
+{
+    Console.WriteLine($"Congratulations {players.Person1}!");
+    Console.WriteLine("You WON this game!");
+ 
+}
+else Console.WriteLine($"{players.Person1} YOU LOST THIS GAME!");
+//Console.Clear();
+ 
+//Board finished - addded 2022-06-06
